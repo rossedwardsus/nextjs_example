6fc248ecf6767b2099d3253d24a824e44245dde0
Ross Edwards <rossedwards.us@gmail.com>
2023-05-30 19:27:16 -0400

    Added screens and started adding add question

--- src/components/header/page.js
+++ src/components/header/page.js
@@ -1,38 +1,31 @@
-import Image from 'next/image';
-import Head from 'next/head';
-import '../../globals.css';
+import Image from 'next/image'
 
-export default function Home({params}) {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      <Head>
-        <title>
-          iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple
-        </title>
-        <meta
-          name="description"
-          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
-          key="desc"
-        />
-      </Head>
-      <h1>iPhones for Sale</h1>
-      <p>insert a list of iPhones for sale. view question{JSON.stringify(params)}</p>
-
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        comments
-        <br/>
-        {[{comment_id: 1, comment: "a comment"}].map((comment) =>
-
-            <>
-                {comment.comment_id}
-                <br/>
-                {comment.comment}
-            </>
-
-        )}
-        
-
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.js</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
@@ -61,9 +54,6 @@ export default function Home({params}) {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Find in-depth information about Next.js features and API.
-            <br></br>
-            <br/>
-            All questions - view question
           </p>
         </a>
 
